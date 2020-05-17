b1310adb2f1c2e995b9c231b876d031bc92a4acf
sezeresim <sezeresim@gmail.com>
2020-05-18 01:47:20 +0300

    assafsa

--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8',
+            'charset' => 'utf8mb4',
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'require',
