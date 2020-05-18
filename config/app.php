ca099aaed8af59daec8e3e4bdc3d3a923f853492
sezeresim <sezeresim@gmail.com>
2020-05-18 16:14:36 +0300

    sadsad

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         //For APİ
-      Laravel\Passport\PassportServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
